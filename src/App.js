452d67cedb539cf745eb323763ac2f54e5118584
Dj <rodriguezdanilo88@gmail.com>
2018-03-15 18:40:16 -0700

    project start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='container'>
+        <div className='logo'></div>
+        <div className='bg bg-light p-2'>
+          <h1 className='display-4 font-weight-bold'>Welcome to Marvel Finder!</h1>
+          <p className='font-weight-bold'>Search the Marvel Universe.</p>
+        </div>
       </div>
     );
   }
